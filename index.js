354bc42e6ce346771da5b75ef737d51598d27352
ladyzhenskii <ladyzhenskii@gmail.com>
2019-07-26 01:55:07 +0300

    Added welcome keyboard
    Added start/dev scripts

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
+
 const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
 const config = require('./config/defaults');
 const locale = require('./constants/locale');
+const { getWelcomeLocale, createKeyboard } = require('./utils');
 
 const bot = new Telegraf(config.token);
 
+const welcomeLocale = getWelcomeLocale(locale);
+
 bot.start((context) => {
-  return context.reply(locale.scenarios.welcome.text);
+  const showWelcomeKeyboard = createKeyboard([
+    Markup.callbackButton(welcomeLocale.buttons.rolePadavan, 'test'),
+    Markup.callbackButton(welcomeLocale.buttons.roleMentor, 'test'),
+  ]);
+
+  return context.reply(welcomeLocale.text, showWelcomeKeyboard);
 });
 
 bot.launch();
