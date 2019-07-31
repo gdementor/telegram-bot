7188ba860fb5cd77bae08e8cc74165efa1f5650c
Deadit <stdeadit@gmail.com>
2019-07-31 13:50:33 +0300

    fix eslint config

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Telegraf = require('telegraf');
-const Markup = require('telegraf/markup');
-const config = require('./config/defaults');
-const locale = require('./constants/locale');
-const { getWelcomeLocale, createKeyboard } = require('./utils');
+const Telegraf = require("telegraf");
+const Markup = require("telegraf/markup");
+const config = require("./config/defaults");
+const locale = require("./constants/locale");
+const { getWelcomeLocale, createKeyboard } = require("./utils");
 
 const bot = new Telegraf(config.token);
 
 const welcomeLocale = getWelcomeLocale(locale);
 
-bot.start((context) => {
-  const showWelcomeKeyboard = createKeyboard([
-    Markup.callbackButton(welcomeLocale.buttons.rolePadavan, '/start'),
-    Markup.callbackButton(welcomeLocale.buttons.roleMentor, '/start'),
-  ], { columns: 2 });
+bot.start(context => {
+  const showWelcomeKeyboard = createKeyboard(
+    [
+      Markup.callbackButton(welcomeLocale.buttons.rolePadavan, "/start"),
+      Markup.callbackButton(welcomeLocale.buttons.roleMentor, "/start")
+    ],
+    { columns: 2 }
+  );
 
   return context.reply(welcomeLocale.text, showWelcomeKeyboard);
 });
